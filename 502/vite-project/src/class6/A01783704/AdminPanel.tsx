f8d47f0e7204ab275a4b6d4e5aac8e6f9e278eef
Dokyu Han <dokyuhan@Dokyus-MacBook-Pro.local>
2025-05-27 20:15:40 -0600

    Entrega de Milestone4

--- 502/vite-project/src/class6/A01783704/AdminPanel.tsx
+++
@@ -1,8 +0,0 @@
-const AdminPanel = () => (
-  <div>
-    <h1 className="text-2xl font-semibold">Admin Panel</h1>
-    <p>Only admins can access this page.</p>
-  </div>
-);
-
-export default AdminPanel;
--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        A01025119: resolve(__dirname, 'A01025119.html')
+      }
+    }
+  }
 })
